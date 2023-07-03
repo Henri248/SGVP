8491d53358c91734e5c19c4f5282277a2fbe138d
andreluizigal <andreluizigal@gmail.com>
2023-07-02 23:44:51 -0300

    adaptacao novo servidor

--- api/handleGET.js
+++ api/handleGET.js
@@ -1,8 +1,6 @@
 const URL = require('url')
 const db = require('../db')
 
-
-
 async function handleGET(req, res) {
     const recurso = URL.parse(req.url, false).pathname;
 
@@ -10,17 +8,122 @@ async function handleGET(req, res) {
         'Access-Control-Allow-Origin': '*',
     });
 
-    switch (recurso) {
-        case ("/produto"):
-            (async () => {
-                const produtos = await db.selectProdutos();
-                //console.log(produtos)
-                //res.send({ produtos: p, email: req.cookies.email, gestor: req.cookies.gestor == 'true' ? true : false })
-                res.end(JSON.stringify(produtos));
-            })();
-            //return res.end(JSON.stringify({"carro": "10"}));
-            
-        
+    switch (true) {
+        // USUARIO
+        case (recurso.startsWith("/perfil")):
+                (async () => {
+                    const partes_url = recurso.split("/");
+                    if (partes_url[2] == "gestor"){
+                        const p = await db.dadosGestor(partes_url[3])
+                        res.end(JSON.stringify(p));
+                    }
+                    else {
+                        const p = await db.dadosUsuario(partes_url[3]);
+                        res.end(JSON.stringify(p));
+                    }
+                })();
+                
+        break;
+        //PRODUTO
+        case (recurso.startsWith("/produto")):
+            if (recurso == "/produto"){
+                (async () => {
+                    const p = await db.selectProdutos();
+                    res.end(JSON.stringify(p));
+                })();
+            } 
+            else {
+                (async () => {
+                    const partes_url = recurso.split("/");
+                    if (partes_url[2] == "delete"){
+                        await db.activeProdutoID(partes_url[3], false)
+                    }
+                    else {
+                        const p = await db.selectProdutoID(partes_url[3]);
+                        res.end(JSON.stringify(p));
+                    }
+                })();
+            } 
+                
+        break;
+
+        // VENDA
+        case (recurso.startsWith("/venda")):
+            if (recurso == "/venda"){
+                (async () => {
+                    const v = await db.selectVendas();
+                    res.end(JSON.stringify(v));
+                })();
+            } 
+            else {
+                (async () => {
+                    const partes_url = recurso.split("/");
+                    if (partes_url[2] == "delete"){
+                        await db.deleteVendaID(partes_url[3])
+                    }
+                    else if (partes_url[2] == "ver"){
+                        const v = await db.selectVendaID(partes_url[3]);
+                        res.end(JSON.stringify(v));
+                    }
+                    else if (partes_url[2] == "produtos"){
+                        const pv = await db.selectProdutosVenda(partes_url[3]);
+                        res.end(JSON.stringify(pv));
+                    }
+                    
+                })();
+            } 
+
+        break;
+
+        // VENDEDOR
+        case (recurso.startsWith("/vendedor")):
+            if (recurso == "/vendedor"){
+                (async () => {
+                    const p = await db.selectVendedores();
+                    res.end(JSON.stringify(p));
+                })();
+            } 
+            else {
+                (async () => {
+                    const partes_url = recurso.split("/");
+                    if (partes_url[2] == "delete"){
+                        await db.activeVendedorID(partes_url[3], false)
+                    }
+                    else if (partes_url[2] == "ver" || partes_url[2] == "editar"){
+                        const v = await db.selectVendedorID(partes_url[3]);
+                        console.log(v)
+                        res.end(JSON.stringify(v));
+                    }
+                    else if (partes_url[2] == "vendas"){
+                        const vd = await db.selectVendasVendedorID(partes_url[3]);
+                        res.end(JSON.stringify(vd));
+                    }
+                    
+                })();
+            } 
+        break;
+        // CLIENTE
+        case (recurso.startsWith("/cliente")):
+            if (recurso == "/cliente"){
+                (async () => {
+                    const p = await db.selectClientes();
+                    res.end(JSON.stringify(p));
+                })();
+            } 
+            else {
+                (async () => {
+                    const partes_url = recurso.split("/");
+                    if (partes_url[2] == "delete"){
+                        console.log("TESTEEEEEEEE", partes_url);
+                        await db.activeProdutoID(partes_url[3], false)
+                    }
+                    else {
+                        const p = await db.selectProdutoID(partes_url[3]);
+                        res.end(JSON.stringify(p));
+                    }
+                })();
+            } 
+        break;
     }
 }
 
--- server.js
+++ server.js
@@ -44,7 +44,25 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 
 
+// Funções ------------------------------------------
 
+async function fetchGet(url) {
+    return await fetch(url)
+        .then(response => response.json())
+        .catch(err => console.log(err));
+}
+
+async function fetchPost(url, req, res) {
+    
+    return await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(req.body),
+        headers: { "Content-type": "application/json" }
+      }).then(response => response.json())
+        .catch(err => console.log(err));;
+    
+}
+  
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
@@ -80,11 +98,14 @@ app.get('/login', (req, res) => {
 
 // POST -> Login
 app.post('/verifica_login', (req, res) => {
-    const { email, senha } = req.body;
+    console.log(req.body);
     (async () => {
-        const login = await db.validarUsuario(email, senha);
+        const login = await fetchPost("http://localhost:9012/login", req, res)
+        console.log("TESTEEEEE", login)
+        
+        console.log(login)
         if (login[0]) {
-            res.cookie('email', email);
+            res.cookie('email', req.body.email);
             res.cookie('gestor', login[1]);
             res.redirect('/produto')
         } else {
@@ -105,48 +126,54 @@ app.get('/perfil', (req, res) => {
     if (req.cookies.email) {
         if (req.cookies.gestor == 'true') {
             (async () => {
-                const u = await db.dadosGestor(req.cookies.email);
+                const u = await fetchGet(`http://localhost:9012/perfil/gestor/${req.cookies.email}`);
                 res.render('pages/perfilGestor', {usuario: u, email: req.cookies.email, gestor: req.cookies.gestor=='true' ? true : false })
             })();
         }
         else {
             (async () => {
-                const u = await db.dadosUsuario(req.cookies.email);
-                const vd = await db.selectVendasVendedorID(u[0].id);
+                const u = await fetchGet(`http://localhost:9012/perfil/vendedor/${req.cookies.email}`);
+                console.log("TESTANDODODODODODO", u, req.cookies.email)
+                const vd = await fetchGet(`http://localhost:9012/vendedor/vendas/${u[0].id}`);
                 res.render('pages/perfil', {usuario: u, vendas: vd, email: req.cookies.email, gestor: req.cookies.gestor=='true' ? true : false })
             })();
         }
     } else {res.send('ACESSO NEGADO!')}
 })
 
-// POST -> Perfil
-app.post('/perfil/post', (req, res) => {
+// POST -> Perfil (VENDEDOR)
+app.post('/perfil/vendedor', upload.single('imagem'), (req, res) => {
     if (req.cookies.email) {
-        if (req.cookies.gestor == 'true') {
-            (async () => {
-                let email = req.cookies.email
-                let novo_email = req.body.email
-                let senha = req.body.senha
-
-                await db.updateGestor(email, novo_email, senha);
-                res.cookie('email', novo_email)
-                res.redirect('/perfil')
-                })();
-        }
-        else {
-            (async () => {
-                let email = req.cookies.email
-                let novo_email = req.body.email
-                let senha = req.body.senha
-                let nome = req.body.nome
-                let telefone = req.body.telefone
-                let endereco = req.body.endereco
         
-                await db.updateUsuario(email, nome, novo_email, senha, telefone, endereco);
-                res.cookie('email', novo_email)
-                res.redirect('/perfil')
-                })();
-        }
+        (async () => {
+        if('file' in req) {req.body.filename = req.file.filename;}
+        req.body.novo_email = req.body.email
+        req.body.email = req.cookies.email
+
+        await fetchPost(`http://localhost:9012/perfil/vendedor/${req.cookies.email}`, req, res)
+            .then(res.cookie('email', req.body.novo_email))
+            .then(res.redirect('/produto'))
+            .catch(res.render('Erro ao editar o Perfil'))
+        })();
+
+    } else {res.send('ACESSO NEGADO!')}
+
+})
+
+// POST -> Perfil (GESTOR)
+app.post('/perfil/gestor', (req, res) => {
+    if (req.cookies.gestor == 'true') {
+        
+        (async () => {
+        req.body.novo_email = req.body.email
+        req.body.email = req.cookies.email
+
+        await fetchPost(`http://localhost:9012/perfil/gestor/${req.cookies.email}`, req, res)
+            .then(res.cookie('email', req.body.novo_email))
+            .then(res.redirect('/produto'))
+            .catch(res.render('Erro ao editar o Perfil'))
+        })();
+
     } else {res.send('ACESSO NEGADO!')}
 
 })
@@ -154,34 +181,14 @@ app.post('/perfil/post', (req, res) => {
 
 // PRODUTO --------------------------------------------------------------
 
-// GET -> Produtos
-app.get('/produtos', (req, res) => {
-
-    (async () => {
-        const p = await db.selectProdutos();
-        //console.log(p)
-
-        res.send({ produtos: p, email: req.cookies.email, gestor: req.cookies.gestor == 'true' ? true : false })
-    })();
-    //res.render('pages/produto/produtos', { produtos: pa, email: req.cookies.email ,gestor: req.cookies.gestor=='true' ? true : false })
-})
-
-
-async function fetchGet(url) {
-    return await fetch(url)
-        .then(response => response.json())
-        .catch(err => console.log(err));
-}
-
+// GET -> /produto
 app.get('/produto', (req, res) => {
 
     (async () => {
-        //const p = await db.selectProdutos();
         const p = await fetchGet('http://localhost:9012/produto');
-        //console.log(p)
 
         res.render('pages/produto/produtos', { produtos: p, email: req.cookies.email, gestor: req.cookies.gestor == 'true' ? true : false })
-        //res.send({ produtos: p, email: req.cookies.email, gestor: req.cookies.gestor == 'true' ? true : false })
+        
     })();
 
 })
@@ -191,7 +198,7 @@ app.get('/produto', (req, res) => {
 app.get('/produto/ver/:p', (req, res) => {
     (async () => {
 
-        const p = await db.selectProdutoID(req.params.p);
+        const p = await fetchGet(`http://localhost:9012/produto/ver/${req.params.p}`);
 
         res.render('pages/produto/verProduto', { produto: p, email: req.cookies.email, gestor: req.cookies.gestor == 'true' ? true : false })
     })();
@@ -204,51 +211,16 @@ app.get('/produto/criar', (req, res) => {
     else {res.send('ACESSO NEGADO!')}
 })
 
-async function fetchPost(url, req, res) {
-    await fetch(url, {
-        method: "POST",
-        body: JSON.stringify(req.body),
-        headers: { "Content-type": "application/json" }
-    })
-        .then(response => response.json())
-        .then(json => console.log(json))
-        .catch(err => console.log(err));
-}
 
 // POST -> Produto/Criar
 app.post('/produto/criar', upload.single('imagem'), (req, res) => {
     if (req.cookies.gestor == 'true') {
 
-        req.body.filename = req.file.filename;
-        //console.log(req.file, req.body);
-        //console.log(os.tmpdir())
-        console.log(req.file, req.body);
+        if('file' in req) {req.body.filename = req.file.filename;}
+
         fetchPost("http://localhost:9012/produto/criar", req, res)
             .then(res.redirect('/produto'))
             .catch(res.render('Erro ao criar o Produto'))
-
-
-
-        //const responseJson = response.json();
-        //console.log(responseJson);
-        //res.send(JSON.stringify(req.body))
-
-
-
-        //res.redirect('/produto/criar')
-        /*
-        (async () => {
-            let nome = req.body.nome
-            let categoria = req.body.categoria
-            let preco = req.body.valor
-            let estoque = req.body.estoque
-            let descricao = req.body.descricao
-
-            await db.insertProduto(nome, categoria, preco, estoque, descricao);
-
-            res.redirect('/produto')
-        })();
-*/
 
     } else {res.send('ACESSO NEGADO!')}
 
@@ -259,7 +231,7 @@ app.get('/produto/editar/:p', (req, res) => {
     if (req.cookies.gestor == 'true') {
         (async () => {
 
-            const p = await db.selectProdutoID(req.params.p);
+            const p = await fetchGet(`http://localhost:9012/produto/editar/${req.params.p}`);
 
             res.render('pages/produto/editarProduto', { produto: p, email: req.cookies.email, gestor: req.cookies.gestor == 'true' ? true : false })
         })();
@@ -268,15 +240,14 @@ app.get('/produto/editar/:p', (req, res) => {
 })
 
 // POST -> Produto/Editar
-app.post('/produto/editar/post', (req, res) => {
+app.post('/produto/editar', upload.single('imagem'), (req, res) => {
     if (req.cookies.gestor == 'true') {
-        //req.body.filename = req.file.filename;
-        //console.log(req.file, req.body);
-        //console.log(os.tmpdir())
-        console.log(req.file, req.body);
-        fetchPost("http://localhost:9012/produto/editar", req, res)
+        
+        if('file' in req) {req.body.filename = req.file.filename;}
+
+        fetchPost(`http://localhost:9012/produto/editar/${req.body.id}`, req, res)
             .then(res.redirect('/produto'))
-            .catch(res.render('Erro ao criar o Produto'))
+            .catch(res.render('Erro ao editar o Produto'))
 
 
     } else {res.send('ACESSO NEGADO!')}
@@ -286,7 +257,8 @@ app.post('/produto/editar/post', (req, res) => {
 app.get('/produto/delete/:id', (req, res) => {
     if (req.cookies.gestor == 'true') {
         (async () => {
-            await db.activeProdutoID(req.params.id, false);
+            fetchGet(`http://localhost:9012/produto/delete/${req.params.id}`)
+        
             res.redirect('/produto')
         })();
 
@@ -298,7 +270,7 @@ app.get('/produto/delete/:id', (req, res) => {
 // GET -> Vendas
 app.get('/venda', (req, res) => {
     (async () => {
-        const v = await db.selectVendas();
+        const v = await fetchGet('http://localhost:9012/venda')
         res.render('pages/venda/vendas', { vendas: v, email: req.cookies.email, gestor: req.cookies.gestor == 'true' ? true : false })
     })();
 })
@@ -307,20 +279,20 @@ app.get('/venda', (req, res) => {
 app.get('/venda/ver/:v', (req, res) => {
     (async () => {
 
-        const v = await db.selectVendaID(req.params.v);
-        const pv = await db.selectProdutosVenda(req.params.v);
+        const v = await fetchGet(`http://localhost:9012/venda/ver/${req.params.v}`);
+        const pv = await fetchGet(`http://localhost:9012/venda/produtos/${req.params.v}`);
 
         res.render('pages/venda/verVenda', { venda: v, produtos_venda: pv, email: req.cookies.email, gestor: req.cookies.gestor == 'true' ? true : false })
     })();
 })
 
-// GET -> Venda/Criar (implementar)
+// GET -> Venda/Criar 
 app.get('/venda/criar', (req, res) => {
 
     if (req.cookies.email) {(async () => {
-        const v = await db.selectVendedores();
-        const c = await db.selectClientes();
-        const p = await db.selectProdutos();
+        const v = await fetchGet(`http://localhost:9012/vendedor`);
+        const c = await fetchGet(`http://localhost:9012/cliente`);
+        const p = await fetchGet(`http://localhost:9012/produto`);
         res.render('pages/venda/criarVenda', { vendedores: v, clientes: c, produtos: p, email: req.cookies.email, gestor: req.cookies.gestor=='true' ? true : false })
         })();
     } else {res.send('ACESSO NEGADO!')}
@@ -331,24 +303,14 @@ app.get('/venda/criar', (req, res) => {
 app.post('/Venda/criar/post', (req, res) => {
     if (req.cookies.email) {
         (async () => {
-        let vendedor = req.body.vendedor;
-        let cliente = req.body.cliente;
         let produtos = req.body.produto_venda;
         typeof(produtos) == 'string' ? produtos = [parseInt(produtos)] : undefined;
         let quantidades = req.body.quantidade;
         typeof(quantidades) == 'string' ? quantidades = [parseInt(quantidades)] : undefined;
         
-        venda = await db.insertVenda(vendedor, cliente);
-        
-        let contador = 0;
-        produtos.forEach(produto => {
-            db.insertProdutoVenda(venda[0].id, produto, quantidades[contador]);
-            contador++
-        });
-
-        
-        
-        res.redirect('/venda')
+        venda = await fetchPost("http://localhost:9012/venda/criar", req, res)
+                            .then(res.redirect('/venda'))
+                            .catch(res.render('Erro ao criar a Venda'))
         })();
     } else {res.send('ACESSO NEGADO!')}
 })
@@ -357,7 +319,7 @@ app.post('/Venda/criar/post', (req, res) => {
 app.get('/venda/delete/:id', (req, res) => {
     if (req.cookies.gestor == 'true') {
         (async () => {
-            await db.deleteVendaID(req.params.id);
+            fetchGet(`http://localhost:9012/venda/delete/${req.params.id}`)
             res.redirect('/venda')
         })();
 
@@ -370,7 +332,7 @@ app.get('/venda/delete/:id', (req, res) => {
 app.get('/vendedor', (req, res) => {
     if (req.cookies.gestor == 'true') {
         (async () => {
-            const v = await db.selectVendedores();
+            const v = await fetchGet(`http://localhost:9012/vendedor`);
             res.render('pages/vendedor/vendedores', { vendedores: v, email: req.cookies.email, gestor: req.cookies.gestor == 'true' ? true : false })
         })();
 
@@ -382,8 +344,8 @@ app.get('/vendedor', (req, res) => {
 app.get('/vendedor/ver/:v', (req, res) => {
     (async () => {
 
-        const v = await db.selectVendedorID(req.params.v);
-        const vd = await db.selectVendasVendedorID(req.params.v);
+        const v = await fetchGet(`http://localhost:9012/vendedor/ver/${req.params.v}`);
+        const vd = await fetchGet(`http://localhost:9012/vendedor/vendas/${req.params.v}`);
 
 
         res.render('pages/vendedor/verVendedor', { vendedor: v, vendas: vd, email: req.cookies.email, gestor: req.cookies.gestor=='true' ? true : false })
@@ -398,19 +360,19 @@ app.get('/vendedor/criar', (req, res) => {
 })
 
 // POST -> Vendedor/Criar
-app.post('/vendedor/criar/post', (req, res) => {
+app.post('/vendedor/criar', upload.single('imagem'), (req, res) => {
     if (req.cookies.gestor == 'true') {
-        (async () => {
-            let nome = req.body.nome
-            let email = req.body.email
-            let telefone = req.body.telefone
-            let endereco = req.body.endereco
-            let meta = req.body.meta
+       
 
-            await db.insertVendedor(nome, email, telefone, endereco, meta, 0);
+            if('file' in req) {req.body.filename = req.file.filename;}
 
+            fetchPost("http://localhost:9012/vendedor/criar", req, res)
+            .then(res.redirect('/vendedor'))
+            .catch(res.render('Erro ao criar o Vendedor'))
+            
             res.redirect('/vendedor')
-        })();
+
+            
 
     } else {res.send('ACESSO NEGADO!')}
 })
@@ -419,7 +381,8 @@ app.post('/vendedor/criar/post', (req, res) => {
 app.get('/vendedor/editar/:v', (req, res) => {
     if (req.cookies.gestor == 'true') {
         (async () => {
-            const v = await db.selectVendedorID(req.params.v);
+            const v = await fetchGet(`http://localhost:9012/vendedor/editar/${req.params.v}`);
+
             res.render('pages/vendedor/editarVendedor', { vendedor: v, email: req.cookies.email, gestor: req.cookies.gestor == 'true' ? true : false })
         })();
 
@@ -427,16 +390,12 @@ app.get('/vendedor/editar/:v', (req, res) => {
 })
 
 // POST -> Vendedor/Editar
-app.post('/vendedor/editar/post', (req, res) => {
+app.post('/vendedor/editar', (req, res) => {
     if (req.cookies.gestor == 'true') {
         (async () => {
-            let id = req.body.id
-            let meta = req.body.meta
-            let meta_atual = req.body.meta_atual
-
-            await db.updateMetaVendedorID(id, meta, meta_atual);
-
-            res.redirect('/vendedor')
+            fetchPost(`http://localhost:9012/vendedor/editar/${req.body.id}`, req, res)
+            .then(res.redirect('/vendedor'))
+            .catch(res.render('Erro ao editar o vendedor'))
         })();
 
     } else {res.send('ACESSO NEGADO!')}
@@ -445,10 +404,9 @@ app.post('/vendedor/editar/post', (req, res) => {
 // GET -> Vendedor/Deletar
 app.get('/vendedor/delete/:id', (req, res) => {
     if (req.cookies.gestor == 'true') {
-        (async () => {
-            await db.activeVendedorID(req.params.id, false);
-            res.redirect('/vendedor')
-        })();
+            
+        fetchGet(`http://localhost:9012/vendedor/delete/${req.params.id}`)
+        res.redirect('/vendedor')
 
     } else {res.send('ACESSO NEGADO!')}
 })
